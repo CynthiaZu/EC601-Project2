81128e12be04ed6da3c47a08837e1a61ef68f006
CynthiaZu <113192414+CynthiaZu@users.noreply.github.com>
2022-10-06 14:20:48 -0400

    Add files via upload

---
+++ twitter_test.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import requests
+import os
+import json
+
+# To set your environment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+#bearer_token = os.environ.get("BEARER_TOKEN")
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAC06hwEAAAAAvPxjhgb%2Bhk1vWaxiczLNCAh0O%2Bk%3DKKLqYYd90j2jzRMerRa4x88qTIiY1KemIm55OVE5cN64kanxmn"
+
+
+def create_url():
+    # Replace with user ID below
+    user_id = 2244994945
+    return "https://api.twitter.com/2/users/{}/followers".format(user_id)
+
+
+def get_params():
+    return {"user.fields": "created_at"}
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2FollowersLookupPython"
+    return r
+
+
+def connect_to_endpoint(url, params):
+    response = requests.request("GET", url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+    return response.json()
+
+
+def main():
+    url = create_url()
+    params = get_params()
+    json_response = connect_to_endpoint(url, params)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
